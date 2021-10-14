b141434742060ed66c2ea5e87c589bbf4cc4aaef
SeriousHyena <emackiii@gmail.com>
2021-10-14 07:17:51 -0700

    added the  "square" function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //declare functions
 void printHello();			//returns nothing
 void printNumber(int a);	//returns nothing
+int square(int sqr);
 int giveMe10();
 int addThese(int num1, int num2);
 
@@ -12,14 +13,20 @@ int main()
 {
 	int num1 = 120;
 	int num2 = 580;
+	int sqr = 9;
 
 	printHello();
 	printNumber(100);
+	square(sqr);
 
 	int someData = giveMe10();
 	cout << someData << endl;
 	int totalValue = addThese(num1, num2);
 	cout << totalValue << endl;
+	cout <<"The square function returned: " <<sqr<< " squared " <<"which is: " << square(sqr) << endl;
+
+	printNumber(addThese(num1, 50));
+
 
 	return 0;
 }
@@ -44,4 +51,10 @@ int addThese(int num1, int num2)
 {
 	int result = num1 + num2;
 	return result;
+}
+
+int square(int sqr)
+{
+	int result2 = sqr * sqr;
+	return result2;
 }
